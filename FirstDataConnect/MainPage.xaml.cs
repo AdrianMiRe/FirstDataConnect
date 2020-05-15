8bbe647f311a67a872a9ada28bf1fa5ca510b5c2
Adrian Miranda Reyes <amiranda@ax.opticalux.com.mx>
2020-05-15 11:21:35 -0500

    Agregar archivos de proyecto.

---
+++ FirstDataConnect/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace FirstDataConnect
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPageLogic Logic { get; } = new MainPageLogic();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ FirstDataConnect/MainPageLogic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PCLService;
+using PCLUtilities;
+using System.Management;
+using System.Threading;
+
+namespace FirstDataConnect
+{
+    public class MainPageLogic : ObservableObject
+    {
+        private ObservableCollection<CompanionInfo> _PairedDevices;
+        public ObservableCollection<CompanionInfo> PairedDevices
+        {
+            get => _PairedDevices;
+            set => Set(ref _PairedDevices, value);
+        }
+
+        public void RefreshDevices()
+        {
+            RefreshDevicesList();
+        }
+
+        private async void RefreshDevicesList()
+        {
+            ObservableCollection<CompanionInfo> listPerUSB = null;
+            ObservableCollection<CompanionInfo> temporalList = new ObservableCollection<CompanionInfo>();
+            
+            try
+            {
+                listPerUSB = new ObservableCollection<CompanionInfo>(await PclUtilities.GetUSBDevices());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("GetUSBDevices : 0x" + ex.HResult.ToString("X") + ", " + ex.Message);
+            }
+
+            if (listPerUSB != null)
+            {
+                foreach (var peer in listPerUSB)
+                    temporalList.Add(peer);
+            }
+
+            PairedDevices = temporalList;
+        }
+    }
+}
